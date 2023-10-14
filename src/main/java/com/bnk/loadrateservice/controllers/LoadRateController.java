a0e56136624197d3084e533de90fb09af3e70d07
sepxx <mihartthx@gmail.com>
2023-10-14 21:55:57 +0300

    port changes

--- src/main/java/com/bnk/loadrateservice/controllers/LoadRateController.java
+++ src/main/java/com/bnk/loadrateservice/controllers/LoadRateController.java
@@ -6,10 +6,7 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -29,7 +26,7 @@ public class LoadRateController {
         return loadRateService.getLoadRateByOfficeId(id,faceType);
     }
 
-    @GetMapping("/offices/load/rating/{faceType}")
+    @PostMapping("/offices/load/rating/{faceType}")
     public Map<Long, Double> getLoadRatesByOfficesIds(@RequestBody Ids ids, @PathVariable String faceType) {
         log.info("getLoadRateByOfficeId(), ids:{}, faceType:{}",ids,faceType);
         List<Long> idsList = ids.getIds();
